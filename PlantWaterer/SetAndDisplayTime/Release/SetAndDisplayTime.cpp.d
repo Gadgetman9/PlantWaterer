305f318e14604f482ab8c9c4e6f3a2495059a566
Gadgetman9 <33383892+Gadgetman9@users.noreply.github.com>
2017-11-04 20:53:22 -0400

    Add files via upload

---
+++ PlantWaterer/SetAndDisplayTime/Release/SetAndDisplayTime.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\Gadgetman\AppData\Local\Temp\VMBuilds\SetAndDisplayTime\uno\Release\SetAndDisplayTime.cpp.o: \
+ C:\Users\Gadgetman\AppData\Local\Temp\VMBuilds\SetAndDisplayTime\uno\Release\SetAndDisplayTime.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Inventions\sketches\libraries\RTClib/RTClib.h
